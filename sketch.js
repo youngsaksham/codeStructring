8ce20af675b554d8d366aca360acfab08e4d81bc
youngsaksham <youngsaksham@gmail.com>
2020-10-05 03:36:30 -0700

    added

--- sketch.js
+++ sketch.js
@@ -1,29 +1,18 @@
-var ball;
+ var canvas,backroundImg;
+ var gameState = 0;
+ var playerCount;
+ var database,form,player,game;
 
-function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+function setup(){ 
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
+    
+     }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
-}
+ 
